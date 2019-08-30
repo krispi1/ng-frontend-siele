30fa648e2fac07df99507b5c79a94c49ae6dddfa
Geoffrey Siele <sielekrisp@gmail.com>
2019-08-30 10:18:55 +0300

    Remove dead code

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,31 +1,22 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { 
-  HttpClientTestingModule, 
-  HttpTestingController 
+import {
+  HttpClientTestingModule,
 } from '@angular/common/http/testing';
-import { 
-  HttpClient, 
-} from '@angular/common/http';
 
 import { AuthService } from './auth.service';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthService', () => {
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule, RouterTestingModule
        ],
     });
-    httpClient = TestBed.get(HttpClient);
-    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should be created: AuthService', 
+  it('should be created: AuthService',
       inject([AuthService],
       (service: AuthService) => {
     expect(service).toBeTruthy();
