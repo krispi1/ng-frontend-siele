a4d17bebc143b56ec3e2aa047392848e1e9b7e90
Geoffrey Siele <sielekrisp@gmail.com>
2019-08-30 10:25:42 +0300

    Modify setHeaders object

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,14 @@ import { AuthService } from './auth.service';
 export class TokenInterceptorService implements HttpInterceptor {
 
   constructor(private injector: Injector) { }
-  
-  intercept(request, next) {
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authService = this.injector.get(AuthService);
     let tokenizedRequest = request.clone({
       setHeaders: {
-        Authorization: `${authService.getToken()}}`
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept': 'application/json',
+        'Authorization': `${authService.getToken()}`
       }
     })
 
