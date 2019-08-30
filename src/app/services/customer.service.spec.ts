1bbdc0614ed85b5d578cccd1141f46ecffa610f6
Geoffrey Siele <sielekrisp@gmail.com>
2019-08-30 10:19:54 +0300

    Fix failing test

--- src/app/services/customer.service.spec.ts
+++ src/app/services/customer.service.spec.ts
@@ -2,10 +2,11 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CustomerService } from './customer.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CustomerService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ]
+    imports: [ HttpClientTestingModule, RouterTestingModule ]
   }));
 
   it('should be created', () => {
